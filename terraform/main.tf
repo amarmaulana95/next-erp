b8688bf4f7994619449e69237c7124dd1b097794
Amar Maulana <amar.unpak@gmail.com>
2026-09-05 20:48:16 +0700

    refactor: separate terraform provider configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = ">= 1.15.0"
-
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.38"
-    }
-  }
-}
-
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = var.kubernetes_context
-}
-
 resource "kubernetes_namespace" "next_erp_dev" {
   metadata {
     name = var.namespace
---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.15.0"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = var.kubernetes_context
+}
